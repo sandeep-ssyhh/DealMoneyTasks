afd0b8b8a1a959766d392685be07ac0b6c43d50e
ssyhh <sandeepyadav1315@gmail.com>
2020-05-30 20:20:16 +0530

    First commit

---
+++ ImportDataFromDB.js
@@ -0,0 +1,62 @@
+const mongodb = require("mongodb").MongoClient;
+
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
+// let url = "mongodb://username:password@localhost:27017/";
+
+let url = "mongodb://localhost:27017/";
+
+const createCSV = async (req, res) => {
+
+  mongodb.connect(
+    url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    (err, client) => {
+      if (err) throw err;
+
+      client
+        .db("user_db")
+        .collection("usertables")
+        .find({})
+        .toArray((err, data) => {
+          if (err) throw err;
+
+          // console.log(data);
+
+          const csvWriter = createCsvWriter({
+            path: "./uploads/userTableData.csv",
+            header: [{
+                id: "_id",
+                title: "ObjectId"
+              },
+              {
+                id: "ts",
+                title: "ts"
+              },
+              {
+                id: "val",
+                title: "val"
+              }
+            ]
+          });
+
+          csvWriter
+            .writeRecords(data)
+            .then(() =>
+              res.status(200).send({
+                link: "http://localhost:8080/userTableData.csv"
+              })
+              // console.log('file created')
+            );
+
+          client.close();
+        });
+    }
+  );
+}
+
+module.exports = {
+  createCSV
+};
---
+++ server.js
@@ -0,0 +1,80 @@
+// *******Insterted Required Pacages**************************/
+let express = require('express');
+var fs = require("fs");
+const JSONStream = require('JSONStream');
+var mongoose = require('mongoose');
+const path = require("path");
+var bodyParser = require('body-parser');
+
+const csvFile = require('./ImportDataFromDB')
+const taskSchemaTable = require('./userModel');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "uploads")));
+
+//**************************Database Connection***************************** */
+mongoose.connect('mongodb://localhost:27017/user_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+
+
+
+var arrayOfUsers = [];
+
+//********************Post Method API****************************************** */ 
+
+app.post('/uploadJsondata', (req, res) => {
+
+
+  var readerStream = fs.createReadStream('./uploads/THERM0001.json');
+  console.log("File has been read");
+
+
+  //************* Inserting  100 records data from the array to database ************* */
+  readerStream.pipe(JSONStream.parse('*')).on('data', async (userData) => {
+
+    arrayOfUsers.push(userData);
+
+    console.log(` Data uploading  please wait .... `);
+
+    if (arrayOfUsers.length === 99) {
+      readerStream.pause();
+
+      await taskSchemaTable.insertMany(arrayOfUsers);
+      arrayOfUsers = [];
+
+      readerStream.resume();
+
+
+
+    }
+  });
+  // *********************************Inserted rest remaining datas**********/
+
+  readerStream.on('end', async () => {
+    await taskSchemaTable.insertMany(arrayOfUsers); // left over data
+    
+    console.log(`Data uploading  complete `)
+    res.status(200).send('\nImport completed, closing connection... the data');
+
+  });
+
+  
+
+});
+//***********************End Of Post API***********************************************888 */
+
+
+
+//******Fetching data from table to CSV file(API) ********** */
+app.get('/downloadCSV', csvFile.createCSV);
+
+
+//*************Port Running on 3000******************************************* */
+app.listen(8080, () => {
+  console.log("Post started and running on 8080");
+});
